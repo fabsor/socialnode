335335ce32cd7751a4b50aecec986cad2bef8ba0
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-10-21 11:02:05 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+/**
+ * Module dependencies.
+ */
+var comment = require('./lib/comment');
+var db = require('./lib/db');
+var express = require('express');
+
+// Create a connection to the database.
+db.createConnection("default", "localhost", 27017, {
+  auto_reconnect : true
+}, function(con) {
+  var app = module.exports = express.createServer();
+  var commentModel = new comment.CommentModel(con.db);
+  // Configuration
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
+
+  app.configure('development', function() {
+    app.use(express.errorHandler({
+      dumpExceptions : true,
+      showStack : true
+    }));
+  });
+
+  app.configure('production', function() {
+    app.use(express.errorHandler());
+  });
+
+  app.get('/comment/post', function(req, res) {
+    res.send('<form method="post" action="/comment">'
+        + '<input type="text" name="name" />'
+        + '<input type="text" name="mail" />'
+        + '<textarea name="comment"></textarea>'
+        + '<input type="submit" value="Submit" />' + '</form>');
+  });
+
+  app.post('/comment', function(req, res) {
+    commentModel.save(req.body, function(result) {
+      res.redirect('back');
+    });
+  });
+
+  app.get('/comment', function(req, res) {
+    commentModel.index({}, function(comments) {
+      res.json(comments);
+    });
+  });
+
+  app.get('/comment/:id', function(req, res) {
+    commentModel.retrieve(req.params.id, function(comment) {
+      res.json(comment);
+    });
+  });
+
+  app.listen(3000);
+  console.log("Express server listening on port %d in %s mode",
+      app.address().port, app.settings.env);
+
+});

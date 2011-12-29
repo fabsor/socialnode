1d7e02c1176c60dab60f071a22728efc3444b242
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-12-29 13:51:18 +0100

    Configurable connections to the database

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 /**
  * Module dependencies.
  */
+var fs = require('fs');
 var comment = require('./lib/comment');
-var db = require('./lib/db');
 var auth = require('./lib/auth');
 var express = require('express');
 
-// Create a connection to the database.
-db.createConnection('default', 'localhost', 27017, {
-    auto_reconnect: true
-}, function (con) {
+// Get our configuration for the database. Once that is done, we can
+// get to work on getting a connection.
+fs.readFile("config/db.json", function(error, data) {
+    if (error) {
+        throw Error('Could not read json file');
+    }
+    var db_settings = JSON.parse(data);
+    var db = require(db_settings.driver);
+    // Create a connection to the database.
+    db.createConnection('default', db_settings, configureApp);
+});
+
+/**
+ * Configure our express app.
+ */
+function configureApp(con) {
   var app = module.exports = express.createServer(),
   commentModel = new comment.CommentModel(con.db);
   // Configuration
@@ -63,4 +75,4 @@ db.createConnection('default', 'localhost', 27017, {
   app.listen(3000);
   console.log('Express server listening on port %d in %s mode',
 		app.address().port, app.settings.env);
-});
+}

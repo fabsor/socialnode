165d285c3b2335000a95c0790dc02f2f93ae687e
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-12-30 15:12:07 +0100

    Conditional sending

--- app.js
+++ app.js
@@ -9,14 +9,80 @@ var express = require('express');
 // Get our configuration for the database. Once that is done, we can
 // get to work on getting a connection.
 fs.readFile("config/db.json", function(error, data) {
-    if (error) {
-        throw Error('Could not read json file');
-    }
-    var db_settings = JSON.parse(data);
-    var db = require(db_settings.driver);
-    // Create a connection to the database.
-    db.createConnection('default', db_settings, configureApp);
+  if (error) {
+    throw Error('Could not read json file');
+  }
+  var db_settings = JSON.parse(data);
+  var db = require(db_settings.driver);
+  // Create a connection to the database.
+  db.createConnection('default', db_settings, configureApp);
 });
+
+/**
+ * Determines if we should send JSON back.
+ */
+var JSONConditional = {}
+
+JSONConditional.accept = function (req) {
+  return req.accepts('application/json');
+}
+
+JSONConditional.send = function (res, data) {
+  res.json(data);
+}
+
+/**
+ * Determines if we should send HTML back.
+ */
+var HTMLConditional = {}
+
+HTMLConditional.accept = function (req) {
+  return req.accepts('html') && req.accepts('text/html');
+}
+
+HTMLConditional.send = function (res, data, options) {
+  // Do we have a suggested template to use?
+  if (typeof options == "object"  && options.template) {
+    template = options.template;
+  }
+  else if (typeof options == "string") {
+    template = options;
+  }
+  if (template) {
+    res.render(template, data);
+  }
+  else {
+    // Otherwise, just send the payload
+    res.send(data);
+  }
+}
+
+/**
+ * Middleware definition for conditional responses.
+ */
+function conditionalResponse() {
+  // A number of conditions that can apply for requests
+  // that can be returned in different ways.
+  var sendConditionals = [JSONConditional, HTMLConditional];
+
+  /**
+   * Return different things depending on the current configuration
+   * and the request headers.
+   */
+  function conditionalSend(req, res, data, options) {
+    for (var i in sendConditionals) {
+        if (sendConditionals[i].accept(req)) {
+            sendConditionals[i].send(res, data, options);
+          break;
+        }
+    }
+}
+
+  return function conditionalResponse(req, res, next) {
+    res.conditionalSend = conditionalSend;
+    next();
+  }
+}
 
 /**
  * Configure our express app.
@@ -32,8 +98,11 @@ function configureApp(con) {
     app.use(express.cookieParser());
     app.use(express.session({ secret: "Some secret thing" }));
     app.use(express.methodOverride());
+    app.use(conditionalResponse());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
+    // Set a default title.
+    app.set('view options', { layout: true, title: 'Social node' });
   });
   app.configure('development', function () {
     app.use(express.errorHandler({
@@ -56,21 +125,22 @@ function configureApp(con) {
   app.post('/login', auth.authenticate);
 
   app.post('/comment', function(req, res) {
-	commentModel.save(req.body, function(result) {
+      commentModel.save(req.body, function(result) {
 	    res.redirect('back');
 	});
     });
 
   app.get('/comment', function(req, res) {
-    commentModel.index({}, function(comments) {
-	    res.json(comments);
+    // Set a suggested template that we can return if we want HTML.
+    commentModel.index({}, function(data) {
+        console.log(data);
+        res.conditionalSend(req, res, { comments: data }, 'comments.jade');
     });
   });
-
   app.get('/comment/:id', function(req, res) {
     commentModel.retrieve(req.params.id, function(comment) {
-	    res.json(comment);
-      });
+      res.conditionalSend(req, res, data, 'comment.jade')
+    });
   });
   app.listen(3000);
   console.log('Express server listening on port %d in %s mode',
